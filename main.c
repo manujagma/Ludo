41742d54e642ac8cb4d487b0c8abcea9009cd0dd
manujagma <manujagma@gmail.com>
2024-08-18 09:28:11 +0530

    secound day

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
+#include "main.h"
 #define home -1
 #define startPoint 0
 #define numPlayers 4
-//player pieces
+#define Borad_size 51//player pieces
+#define greenHome 0000//please assign value
 typedef struct 
 {
     int piecesPosition;
@@ -16,7 +18,14 @@ typedef struct Player
 {
     Pieces pieces[4];//make 4 for pieces for one player
     char playercolor;
+    int startGamePieces;
 }Player;
+typedef struct Borad
+{
+    int boradNum;
+    int piecesNum;
+}Borad;
+
 // Call random numbers 
 int roll()
 {
@@ -26,21 +35,116 @@ int roll()
 void playermove();
 int playerstart();
 void playerInstall();
+void greenplayer();
+void yellowPlayer();
+void redPlayer();
+void bluePlayer();
+void playerAiCall();
+void boradconst();
+int bolckPieces();
 
 
+void printPlayers(Player *player) {
+    for (int i = 0; i < numPlayers; i++) {
+        printf("Player %d (Color: %c):\n", i + 1, player[i].playercolor);
+        for (int j = 0; j < 4; j++) {
+            printf("  Piece %d: Position = %d, StartGame = %d\n",
+                   j + 1, player[i].pieces[j].piecesPosition, player[i].pieces[j].startgame);
+        }
+    }
+}
 
 int main()
 {
+    srand(time(NULL));
+    //install player
     Player player[numPlayers];
-    playerInstall(&player);
-
+    playerInstall(player);
+    //make variable for startPlayer
+    int startPlayer=-1;
+    //install borad
+    
+    Borad  borad [Borad_size];
+    for (int i = 0; i < Borad_size; i++)
+    {
+        borad[i].boradNum=i;
+        borad[i].piecesNum=0;
+    }
+    playerstart(&startPlayer);
+    printf("%d\n",startPlayer);
+    //export to header.h (player play method)
+    switch (startPlayer)
+    {
+    case 0:
+        player[0].playercolor='R';
+        player[1].playercolor='G';
+        player[2].playercolor='Y';
+        player[3].playercolor='B';
+        break;
+    case 1:
+        player[0].playercolor='G';
+        player[1].playercolor='Y';
+        player[2].playercolor='B';
+        player[3].playercolor='R';
+        break;
+    case 2:
+        player[0].playercolor='Y';
+        player[1].playercolor='B';
+        player[2].playercolor='R';
+        player[3].playercolor='G';
+        break;
+    case 3:
+        player[0].playercolor='B';
+        player[1].playercolor='R';
+        player[2].playercolor='G';
+        player[3].playercolor='Y';
+        break;   
+    
+    default:
+        break;
+    }
+   // 
+   
+   printPlayers(player);
+    
     
     
 }
-int playerstart()
-{
-    int rollNum =roll();
+int playerstart(int *startPlayer)
+{   
+   
+    int tie=1;
+    while (tie)
+    {
+        tie=0;
+        int a[4];
+        for (int i = 0; i < numPlayers; i++)
+        {
+             int rollNum =roll();
+                a[i]=rollNum;
+        }
+        *startPlayer=0;
+        //shrot this 
+        if (a[1]<a[0] && a[2]<a[0] && a[3]<a[0])
+        {
+            *startPlayer =0; tie=0; 
+        }
+                if (a[0]<a[1] && a[2]<a[1] && a[3]<a[1])
+        {
+            tie=0; *startPlayer=1;
+        }
+                        if (a[0]<a[2] && a[1]<a[2] && a[3]<a[2])
+        {
+            tie=0; *startPlayer=2;
+        }
+                        if (a[0]<a[3] && a[2]<a[3] && a[1]<a[3])
+        {
+            tie=0; *startPlayer=3;
+        }
+        
+    }
     
+
 
 }
 void playerInstall(Player *player)
@@ -62,7 +166,131 @@ for (int  i = 0; i < numPlayers; i++)
 
 
 }
-void playermove()
+//behewire of board
+
+void boradconst(Player *player,Borad *borad)
 {
+    int playerNum =player;
+    int boradNum =borad;
+    int rollNum =0;
+    int piecesNum=0;
+    int picesToHome=0;//can move pices to home
+while (1)
+{
+    
+    rollNum=roll();
+   
+    if (rollNum==6)
+    {
+        
+        playerAiCall(&player,&borad,playerNum,&picesToHome);
+        playermove(&playerNum,&piecesNum,&rollNum,&picesToHome);
+        
+        
+
+    }
+    
+
+
+
+    //stay behind of players and borad (go around like circule)
+    playerNum=(++playerNum)%numPlayers;
+    boradNum=(++boradNum)%Borad_size;
+}
+}
+void playermove(int *playerNum,int *piecesNum,int *rollNum,int *picesToHome)
+{
+    
+     Borad *borad;
+     Player *player;
+     int currentPlayer=*playerNum;
+     
+
+    
+    
+    
+    if (player[currentPlayer].playercolor=='G')
+    {
+        
+        if (picesToHome==1)
+        {
+           player[currentPlayer].pieces[*piecesNum].piecesPosition=greenHome;//please assign value
+           borad[greenHome].piecesNum+=4;//green go on 4,8,12
+        }        
+        if(picesToHome==2)//clockwise go
+        {
+        for (int i = 0; i < *rollNum; i++)
+        {   int currentPiecesPositition=player[currentPlayer].pieces[*piecesNum].piecesPosition;
+            player[currentPlayer].pieces[*piecesNum].piecesPosition=0;//meya palehata enna one
+            currentPiecesPositition++;
+           int bolckBehewivre = bolckPiceces(&borad,&player,currentPiecesPositition,currentPlayer);
+            
+
+            
+            
+            
+
+            //stay in borad size
+            if (player[currentPlayer].pieces[*piecesNum].piecesPosition>=Borad_size)
+            {
+                player[currentPlayer].pieces[*piecesNum].piecesPosition%=Borad_size;
+            }
+        
+        }
+        }
+    }
+       
+    
+    }
+
+int bolckPiceces(Borad *borad,Player *player,int currentPiecesPositition,int currentPlayer)
+{
+    
+    if (player[currentPlayer].playercolor=='G' && borad[currentPiecesPositition].piecesNum==8 )
+    {
+        
+        //print two
+        return 1;
+    }
+    if (player[currentPlayer].playercolor=='G' && borad[currentPiecesPositition].piecesNum==12)
+    {
+        //print three and can't move any pieces to this cell
+        return 0;
+    }
+    if (borad[currentPiecesPositition].piecesNum==8 && player[currentPiecesPositition].playercolor!='G')
+    {
+        //print green block block your way
+        return 0;
+    }
+    if (borad[currentPiecesPositition].piecesNum==12 && player[currentPiecesPositition].playercolor!='G')
+    {
+        //print green super block block your way
+        return 0;
+    }
+    
+    
+    
+}
+void playerAiCall(Player *player,Borad *borad,int num,int *piecesToHome)
+{
+    if (num==1)
+    {
+        greenplayer(&player,&borad,&piecesToHome);
+    }
+    
+}
+void greenplayer(Player *player,Borad *borad,int* piecesToHome)
+{
+    if (*piecesToHome==0)
+    {
+        int piecesWay= rand()%2+1;
+        if (piecesWay==1)
+        {
+            *piecesToHome==2;//make clock wise dirction
+        }else if(piecesWay==2)
+        {
+            *piecesToHome==3;//make counter clock wise dirction
+        }
+    }
 
 }
--- sample.c
+++ sample.c
@@ -2,58 +2,214 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define BOARD_SIZE 52  // Standard Ludo board size
-#define START_POSITION 0
+#define HOME -1
+#define START_POINT 0
 #define NUM_PLAYERS 4
+#define BOARD_SIZE 51
+#define GREEN_HOME 0  // Placeholder value for green home
 
 typedef struct {
-    int position;
-    int hasStarted;
+    int piecesPosition;
+    int startgame;
+} Pieces;
+
+typedef struct {
+    Pieces pieces[4];
+    char playerColor;
+    int startGamePieces;
 } Player;
 
-// Function to roll a dice
-int rollDice() {
-    return (rand() % 6) + 1;
-}
+typedef struct {
+    int boardNum;
+    int piecesNum;
+} Board;
 
-// Function to move a player's token
-void movePlayer(Player* player, int roll) {
-    if (!player->hasStarted) {
-        if (roll == 6) {
-            player->hasStarted = 1;
-            player->position = START_POSITION + 1;
-            printf("Player starts moving! New position: %d\n", player->position);
-        } else {
-            printf("Player needs a 6 to start. Roll was: %d\n", roll);
-        }
-    } else {
-        player->position += roll;
-        if (player->position > BOARD_SIZE) {
-            player->position = BOARD_SIZE;  // Ensuring the position doesn't exceed the board size
-        }
-        printf("Player moves by %d. New position: %d\n", roll, player->position);
-    }
-}
+// Function prototypes
+int roll();
+void initializePlayers(Player *players);
+void initializeBoard(Board *board);
+int determineStartingPlayer();
+void playermove(Player *players, Board *board, int playerNum);
+void playerAiMove(Player *players, Board *board, int playerNum);
+void printPlayers(const Player *players);
+int canMoveToPosition(Player *player, int pieceIndex, int rollNum);
+int canCreateBlock(Player *player, int pieceIndex, int rollNum, Board *board);
+int shouldBreakBlock(Player *player, int pieceIndex, int rollNum, Board *board);
 
 int main() {
-    srand(time(NULL));  // Seed for random number generation
+    srand(time(NULL));
 
-    Player players[NUM_PLAYERS];  // Array to hold the 4 players
-    for (int i = 0; i < NUM_PLAYERS; i++) {
-        players[i].position = 0;
-        players[i].hasStarted = 0;
-    }
+    Player players[NUM_PLAYERS];
+    Board board[BOARD_SIZE];
 
-    int currentPlayer = 0;
+    initializePlayers(players);
+    initializeBoard(board);
 
-    for (int i = 0; i < 20; i++) {  // Simulate 20 moves
-        int roll = rollDice();
-        printf("Player %d's turn. Dice roll: %d\n", currentPlayer + 1, roll);
-        movePlayer(&players[currentPlayer], roll);
+    int startPlayer = determineStartingPlayer();
+    printf("Starting Player: %d\n", startPlayer);
 
-        // Move to the next player
-        currentPlayer = (currentPlayer + 1) % NUM_PLAYERS;
+    printPlayers(players);
+
+    // Example game loop (simplified)
+    while (1) {
+        for (int i = 0; i < NUM_PLAYERS; i++) {
+            if (players[i].playerColor == 'G') {  // Example condition for AI player
+                playerAiMove(players, board, i);
+            } else {
+                playermove(players, board, i);
+            }
+        }
+        // Add conditions to break the loop based on game end conditions
     }
 
     return 0;
+}
+
+int roll() {
+    return (rand() % 6) + 1;
+}
+
+void initializePlayers(Player *players) {
+    char colors[] = {'R', 'G', 'Y', 'B'};
+    for (int i = 0; i < NUM_PLAYERS; i++) {
+        players[i].playerColor = colors[i];
+        players[i].startGamePieces = 0;
+        for (int j = 0; j < 4; j++) {
+            players[i].pieces[j].piecesPosition = HOME;
+            players[i].pieces[j].startgame = -1;
+        }
+    }
+}
+
+void initializeBoard(Board *board) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        board[i].boardNum = i;
+        board[i].piecesNum = 0;
+    }
+}
+
+int determineStartingPlayer() {
+    int rolls[NUM_PLAYERS];
+    int highestRoll = 0;
+    int startPlayer = 0;
+
+    for (int i = 0; i < NUM_PLAYERS; i++) {
+        rolls[i] = roll();
+        if (rolls[i] > highestRoll) {
+            highestRoll = rolls[i];
+            startPlayer = i;
+        }
+    }
+
+    return startPlayer;
+}
+
+void playermove(Player *players, Board *board, int playerNum) {
+    int rollNum = roll();
+    Player *currentPlayer = &players[playerNum];
+    printf("Player %c rolled a %d\n", currentPlayer->playerColor, rollNum);
+
+    // Example movement logic
+    for (int i = 0; i < 4; i++) {
+        if (currentPlayer->pieces[i].piecesPosition != HOME) {
+            int newPosition = (currentPlayer->pieces[i].piecesPosition + rollNum) % BOARD_SIZE;
+            board[currentPlayer->pieces[i].piecesPosition].piecesNum--;
+            currentPlayer->pieces[i].piecesPosition = newPosition;
+            board[newPosition].piecesNum++;
+            break;
+        }
+    }
+}
+
+void playerAiMove(Player *players, Board *board, int playerNum) {
+    Player *currentPlayer = &players[playerNum];
+    int rollNum = roll();
+    printf("AI Player %c rolled a %d\n", currentPlayer->playerColor, rollNum);
+
+    if (currentPlayer->playerColor != 'G') {
+        playermove(players, board, playerNum);  // Placeholder for other AI logic
+        return;
+    }
+
+    // Logic for green player
+    if (rollNum == 6) {
+        for (int i = 0; i < 4; i++) {
+            if (currentPlayer->pieces[i].piecesPosition == HOME) {
+                // Move piece out of home if possible
+                currentPlayer->pieces[i].piecesPosition = GREEN_HOME;
+                board[GREEN_HOME].piecesNum++;
+                return;
+            }
+        }
+    }
+
+    // Prioritize moving pieces home or creating blocks
+    for (int i = 0; i < 4; i++) {
+        if (canMoveToPosition(currentPlayer, i, rollNum)) {
+            int newPosition = (currentPlayer->pieces[i].piecesPosition + rollNum) % BOARD_SIZE;
+            board[currentPlayer->pieces[i].piecesPosition].piecesNum--;
+            currentPlayer->pieces[i].piecesPosition = newPosition;
+            board[newPosition].piecesNum++;
+            return;
+        }
+    }
+
+    // Check for block breaking
+    for (int i = 0; i < 4; i++) {
+        if (shouldBreakBlock(currentPlayer, i, rollNum, board)) {
+            int newPosition = (currentPlayer->pieces[i].piecesPosition + rollNum) % BOARD_SIZE;
+            board[currentPlayer->pieces[i].piecesPosition].piecesNum--;
+            currentPlayer->pieces[i].piecesPosition = newPosition;
+            board[newPosition].piecesNum++;
+            return;
+        }
+    }
+}
+
+int canMoveToPosition(Player *player, int pieceIndex, int rollNum) {
+    int newPosition = player->pieces[pieceIndex].piecesPosition + rollNum;
+    if (newPosition >= BOARD_SIZE) {
+        newPosition %= BOARD_SIZE;
+    }
+    // Add any additional rules for moving to this position
+    return player->pieces[pieceIndex].piecesPosition != HOME;
+}
+
+int canCreateBlock(Player *player, int pieceIndex, int rollNum, Board *board) {
+    int newPosition = player->pieces[pieceIndex].piecesPosition + rollNum;
+    if (newPosition >= BOARD_SIZE) {
+        newPosition %= BOARD_SIZE;
+    }
+    // Check if moving to this position creates a block
+    return board[newPosition].piecesNum > 1;
+}
+
+int shouldBreakBlock(Player *player, int pieceIndex, int rollNum, Board *board) {
+    int newPosition = player->pieces[pieceIndex].piecesPosition + rollNum;
+    if (newPosition >= BOARD_SIZE) {
+        newPosition %= BOARD_SIZE;
+    }
+    // Check if breaking a block is necessary
+    for (int i = 0; i < 4; i++) {
+        if (player->pieces[i].piecesPosition != HOME && i != pieceIndex) {
+            int forwardPosition = player->pieces[i].piecesPosition + rollNum;
+            if (forwardPosition >= BOARD_SIZE) {
+                forwardPosition %= BOARD_SIZE;
+            }
+            if (board[forwardPosition].piecesNum == 0) {
+                return 0;  // Another piece can move forward without breaking the block
+            }
+        }
+    }
+    return 1;
+}
+
+void printPlayers(const Player *players) {
+    for (int i = 0; i < NUM_PLAYERS; i++) {
+        printf("Player %d (Color: %c):\n", i + 1, players[i].playerColor);
+        for (int j = 0; j < 4; j++) {
+            printf("  Piece %d: Position = %d, StartGame = %d\n",
+                   j + 1, players[i].pieces[j].piecesPosition, players[i].pieces[j].startgame);
+        }
+    }
 }
